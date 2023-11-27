50d1931212be79084b12724a10c80bcdbe7bf362
Silvia-Wachira <slywachira96@gmail.com>
2023-11-27 14:27:18 +0300

    create jsx components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-    </div>
-  );
-}
+  // return (
+  //   <div className="App">
+  //     <h1>Hi, I'm a React App</h1>
+  //   </div>
+  // );
+  return React.createElement('div', {className: 'App'}, React.createElement('h1'), null, 'Does this work now')
+} 
 
 export default App;

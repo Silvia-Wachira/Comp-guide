243b5ad100302c0b4f006e3b976508ee8ef77c41
Silvia-Wachira <slywachira96@gmail.com>
2023-11-27 21:03:51 +0300

    pass props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import person from './Person/Person';
+import Person from './Person/Person';
+// import { render } from '@testing-library/react';
 
 function App() {
+  
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!!!</p>
-      <Person />
+      <Person name="Max" age="28"/>
+      <Person name="Manu" age="29">My Hobbies: Racing</Person>
+      <Person name="Stephanie" age="26"/>
+
+      
+
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1'), null, 'Does this work now')

9907768b25884b4286d3d25205fb980475a682bb
Silvia-Wachira <slywachira96@gmail.com>
2023-11-27 14:58:11 +0300

    create a new component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hi, I'm a React App</h1>
-  //   </div>
-  // );
-  return React.createElement('div', {className: 'App'}, React.createElement('h1'), null, 'Does this work now')
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!!!</p>
+    </div>
+  );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1'), null, 'Does this work now')
 } 
 
 export default App;

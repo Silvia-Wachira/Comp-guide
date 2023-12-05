67b1394fdfbad6cc60e944ee62b47a3af95d4050
Silvia-Wachira <slywachira96@gmail.com>
2023-12-05 16:08:34 +0300

    render components

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Person from './Person/Person';
 
 // function App() {
   class App extends Component {
-    return() {
-      <div className="App" >
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!! </p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29" />
-        <Person name="Stephanie" age="26" />
-
-      </div>
+    render() {
+      return (
+        <div>
+          <h1>Hi, I'm a React App</h1>
+          <p>This is really working!</p>
+          <Person name="Max" age="28" />
+          <Person name="Manu" age="29"> My Hobbies: Racing </Person>
+          <Person name="Stephanie" age="26" />
+        </div>
+      )
     }
   }
 

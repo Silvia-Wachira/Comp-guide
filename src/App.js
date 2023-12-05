1b86b4d98180922a8eb76338de106499d2fc8abc
Silvia-Wachira <slywachira96@gmail.com>
2023-12-05 13:16:49 +0300

    create a class

--- src/App.js
+++ src/App.js
@@ -3,29 +3,17 @@ import './App.css';
 import Person from './Person/Person';
 // import { render } from '@testing-library/react';
 
-function App() {
-  state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stepanhie', age: 26}
-    ]
+// function App() {
+  class App extends Component {
+    return() {
+      <div className="App" >
+
+      </div>
+    }
   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!!!</p>
-      <button>Switch name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
 
-    
-
-    </div>
-  );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1'), null, 'Does this work now')
-} 
+  
 
 export default App;

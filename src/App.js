0ac04139802beab17c74ce54424fafae33b2c62b
Silvia-Wachira <slywachira96@gmail.com>
2023-12-06 17:17:31 +0300

    create a bind

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ import Person from './Person/Person';
       otherState: 'some other value'
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
      // console.log('Was clicked!');
     //  this.state.persons[0] = 'Maximillium'
     this.setState({
       persons: [
-        { name: 'Maximillian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ] })
@@ -30,9 +30,16 @@ import Person from './Person/Person';
           <h1> Hi, I'm a React App </h1>
           <p>This is really working!</p>
           <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} /> 
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Racing </Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          <Person
+           name={this.state.persons[0].name} 
+           age={this.state.persons[0].age} /> 
+          <Person 
+          name={this.state.persons[1].name}
+           age={this.state.persons[1].age}
+           click={this.switchNameHandler.bind(this, 'Max!')}> My Hobbies: Racing </Person>
+          <Person
+           name={this.state.persons[2].name} 
+           age={this.state.persons[2].age} />
         </div>
       );
     }

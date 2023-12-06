51794cd53b627330804662e626b9ee48ea703d48
Silvia-Wachira <slywachira96@gmail.com>
2023-12-06 09:43:54 +0300

    rerender the component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Person from './Person/Person';
 // import { render } from '@testing-library/react';
 
-// function App() {
   class App extends Component {
     state = {
-      persos: [
+      persons: [
         { name: 'Max', age: 28 },
         { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 26 }
+        { name: 'Stephanie', age: 27 }
       ]
     }
 
@@ -19,16 +18,13 @@ import Person from './Person/Person';
           <h1>Hi, I'm a React App</h1>
           <p>This is really working!</p>
           <button>Switch Name</button>
-          <Person name="Max" age="28" />
-          <Person name="Manu" age="29"> My Hobbies: Racing </Person>
-          <Person name="Stephanie" age="26" />
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} /> 
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Racing </Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
         </div>
-      )
+      );
     }
   }
-
-
   // return React.createElement('div', {className: 'App'}, React.createElement('h1'), null, 'Does this work now')
   
-
 export default App;

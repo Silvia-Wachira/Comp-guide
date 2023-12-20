13822a62011a3ae64918d680f4b9034545ad83d7
Silvia-Wachira <slywachira96@gmail.com>
2023-12-20 04:16:00 +0300

    add a slice method

--- src/App.js
+++ src/App.js
@@ -14,10 +14,6 @@ import Person from './Person/Person';
       showPersons: false
     }
 
-    deletePersonHandler = (personIndex) => {
-      
-    }
-
     nameChangedHandler = (event) => {
       this.setState( {
         persons: [
@@ -26,6 +22,12 @@ import Person from './Person/Person';
           { name: 'Stephanie', age: 26 }
         ]
       } )
+    }
+
+    deletePersonHandler = (personIndex) => {
+      const persons = this.state.persons.slice();
+      persons.splice(personIndex, 1);
+      this.setState({persons: persons})
     }
 
     togglePersonsHandler = () => {
@@ -50,7 +52,7 @@ import Person from './Person/Person';
           <div>
             {this.state.persons.map((person, index) => {
               return <Person
-              click={this.deletePersonHandler(index)}
+              click={() => this.deletePersonHandler(index)}
                name={person.name}
                age={person.age} />
             })}

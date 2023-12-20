8bdabde267fb03c59092c37923f07abd608716fb
Silvia-Wachira <slywachira96@gmail.com>
2023-12-20 04:35:13 +0300

    create keys with ids

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Person from './Person/Person';
   class App extends Component {
     state = {
       persons: [
-        { name: 'Max', age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 26 }
+        { id: 'asfa1', name: 'Max', age: 28 },
+        { id: 'vasfd1', name: 'Manu', age: 29 },
+        { id: 'asdf11', name: 'Stephanie', age: 26 }
       ],
       otherState: 'some other value',
       showPersons: false
@@ -25,7 +25,7 @@ import Person from './Person/Person';
     }
 
     deletePersonHandler = (personIndex) => {
-      const persons = this.state.persons.slice();
+      const persons = [...this.state.persons];
       persons.splice(personIndex, 1);
       this.setState({persons: persons})
     }
@@ -54,7 +54,8 @@ import Person from './Person/Person';
               return <Person
               click={() => this.deletePersonHandler(index)}
                name={person.name}
-               age={person.age} />
+               age={person.age} 
+               key={person.index} />
             })}
           </div>
         );

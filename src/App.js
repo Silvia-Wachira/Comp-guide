e6ab3ef115d08407cef829d9c8433dc4af551eb4
Silvia-Wachira <slywachira96@gmail.com>
2023-12-21 21:52:10 +0300

    0

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ import Person from './Person/Person';
         {persons}
         </div>
       );
-    }
+    } 
   }
   // return React.createElement('div', {className: 'App'}, React.createElement('h1'), null, 'Does this work now')
   
